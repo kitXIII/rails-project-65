b40b0ebbb776259a1b959c8a5dd7c77f1499e619
Nikita Konovalov <mipo4t@gmail.com>
2024-03-17 18:25:47 +0400

    add user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,9 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
+  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP },
+                    presence: true,
+                    uniqueness: { case_sensitive: false }
+
+  validates :name, presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_03_16_162421) do
+ActiveRecord::Schema[7.1].define(version: 2024_03_17_141344) do
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["email"], name: "index_users_on_email"
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
