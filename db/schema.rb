f1cf761d5c73e5a6037f52c412507fb53ae66bc1
Nikita Konovalov <mipo4t@gmail.com>
2024-03-17 18:55:48 +0400

    add categories

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_03_17_141344) do
+ActiveRecord::Schema[7.1].define(version: 2024_03_17_144637) do
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["name"], name: "index_categories_on_name", unique: true
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
