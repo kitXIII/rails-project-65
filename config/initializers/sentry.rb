5af37bd67e7de95881a727c81ab1f2202a9a3af2
Nikita Konovalov <mipo4t@gmail.com>
2024-03-27 14:31:22 +0400

    add bulletins controller tests

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -4,6 +4,8 @@
 Sentry.init do |config|
   config.breadcrumbs_logger = %i[active_support_logger http_logger]
 
+  config.enabled_environments = %i[production]
+
   config.excluded_exceptions += [
     'ActionController::RoutingError',
     'ActionController::UnknownFormat',
--- test/controllers/web/bulletins_controller_test.rb
+++ test/controllers/web/bulletins_controller_test.rb
@@ -3,9 +3,98 @@
 require 'test_helper'
 
 class Web::BulletinsControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    @bulletin = bulletins(:one)
+    @author = users(:one)
+    @user = users(:two)
+
+    @attrs = {
+      title: Faker::Book.title,
+      description: Faker::Books::Dune.quote,
+      category_id: categories(:one).id,
+      image: fixture_file_upload('one.jpg', 'image/jpg')
+    }
+  end
+
   test 'should get index' do
     get root_url
 
     assert_response :success
+  end
+
+  test 'should get show' do
+    get bulletin_url(@bulletin)
+
+    assert_response :success
+  end
+
+  test 'should not get new, when user not authorized' do
+    get new_bulletin_url
+
+    assert_redirected_to root_url
+  end
+
+  test 'should get new, when user authorized' do
+    sign_in(@user)
+    get new_bulletin_url
+
+    assert_response :success
+  end
+
+  test 'should not create when user not authorized' do
+    assert_no_difference 'Bulletin.count' do
+      post bulletins_url, params: { bulletin: @attrs }
+    end
+
+    assert_redirected_to root_url
+  end
+
+  test 'should create when user authorized' do
+    sign_in(@user)
+    post bulletins_url, params: { bulletin: @attrs }
+
+    bulletin = Bulletin.find_by(@attrs.except(:image))
+
+    assert { bulletin }
+    assert { bulletin.author == @user }
+    assert_redirected_to bulletin_url(bulletin)
+  end
+
+  test 'should not get edit, when user not authorized' do
+    get edit_bulletin_url(@bulletin)
+
+    assert_redirected_to root_url
+  end
+
+  test 'should not update when user not authorized' do
+    patch bulletin_url(@bulletin), params: { bulletin: @attrs }
+
+    @bulletin.reload
+
+    assert { @bulletin.title != @attrs[:title] }
+    assert { @bulletin.description != @attrs[:description] }
+    assert_redirected_to root_url
+  end
+
+  test 'should not update when user not author' do
+    sign_in(@user)
+    patch bulletin_url(@bulletin), params: { bulletin: @attrs }
+
+    @bulletin.reload
+
+    assert { @bulletin.title != @attrs[:title] }
+    assert { @bulletin.description != @attrs[:description] }
+    assert_response :missing
+  end
+
+  test 'should update bulletin' do
+    sign_in(@author)
+    patch bulletin_url(@bulletin), params: { bulletin: @attrs }
+
+    @bulletin.reload
+
+    assert { @bulletin.title == @attrs[:title] }
+    assert { @bulletin.description == @attrs[:description] }
+    assert_redirected_to bulletin_url(@bulletin)
   end
 end
