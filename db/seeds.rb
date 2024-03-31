e2e368dba80c859a1e1d3d62ced13e0fc7e1f872
Nikita Konovalov <mipo4t@gmail.com>
2024-03-31 18:56:39 +0400

    remove categories seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,1 @@
 # frozen_string_literal: true
-
-categories = ['Для дома', 'Транспорт', 'Недвижимость', 'Хобби и отдых', 'Работа', 'Электроника', 'Животные']
-
-categories.each do |name|
-  Category.find_or_create_by!(name:)
-end
