08e75129e40669ea35e4a7fa93dc339f20d7d62b
Nikita Konovalov <mipo4t@gmail.com>
2024-04-03 11:30:45 +0400

    improve supervisor protection

--- app/controllers/web/admin/users_controller.rb
+++ app/controllers/web/admin/users_controller.rb
@@ -2,20 +2,22 @@
 
 class Web::Admin::UsersController < Web::Admin::ApplicationController
   def index
-    @users = policy_scope(User).where(admin: false).order(name: :asc)
-    @admins = policy_scope(User).where(admin: true).order(name: :asc)
+    @users = User.where(admin: false).order(name: :asc)
+    @admins = User.where(admin: true).order(name: :asc)
   end
 
   def show
-    @user = policy_scope(User).find(params[:id])
+    @user = User.find(params[:id])
   end
 
   def edit
-    @user = authorize User.find(params[:id])
+    @user = User.find(params[:id])
+    authorize @user
   end
 
   def update
-    @user = authorize User.find(params[:id])
+    @user = User.find(params[:id])
+    authorize @user
 
     if @user.update(user_params)
       redirect_to admin_users_path, notice: t('.success')
