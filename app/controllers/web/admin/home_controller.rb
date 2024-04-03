e6b6f2bc81f106956c2d2ddf0b1ae8a8e650a6b2
Nikita Konovalov <mipo4t@gmail.com>
2024-04-03 22:19:07 +0400

    polish admin bulletins

--- app/controllers/web/admin/home_controller.rb
+++ app/controllers/web/admin/home_controller.rb
@@ -3,8 +3,9 @@
 class Web::Admin::HomeController < Web::Admin::ApplicationController
   def index
     @q = Bulletin.under_moderation.ransack(params[:q])
-    @q.sorts = 'created_at desc' if @q.sorts.empty?
+    @q.sorts = 'updated_at desc' if @q.sorts.empty?
 
     @bulletins = @q.result.includes(:author)
+    @categories = Category.order(name: :asc)
   end
 end
