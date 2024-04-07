789d0566a81646961d739e9471dc2e78e6b48a06
Nikita Konovalov <mipo4t@gmail.com>
2024-04-07 18:38:41 +0400

    remove useless policy scope

--- app/policies/user_policy.rb
+++ app/policies/user_policy.rb
@@ -1,12 +1,6 @@
 # frozen_string_literal: true
 
 class UserPolicy < ApplicationPolicy
-  class Scope < ApplicationPolicy::Scope
-    def resolve
-      scope.where.not(id: AdminHelper.supervisor&.id)
-    end
-  end
-
   def edit?
     update?
   end
